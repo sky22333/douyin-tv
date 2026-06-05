dedf18bc1601084dcb70a43071cd22aead99f256
starry <115192496+sky22333@users.noreply.github.com>
2026-06-06 05:20:27 +0800

    Update ShortcutDispatcher.kt

--- app/src/main/java/com/douyin/tv/ShortcutDispatcher.kt
+++ app/src/main/java/com/douyin/tv/ShortcutDispatcher.kt
@@ -1,49 +1,27 @@
 package com.douyin.tv
 
+import android.os.SystemClock
+import android.view.KeyEvent
 import android.webkit.WebView
 
-enum class Shortcut(val key: String, val code: String, val legacyKeyCode: Int) {
-    Previous("w", "KeyW", 87),
-    Next("s", "KeyS", 83),
-    LeftAction("r", "KeyR", 82),
-    RightAction("z", "KeyZ", 90),
-    OkAction("x", "KeyX", 88),
-    PlayerFullscreen("h", "KeyH", 72),
-    WebImmersive("y", "KeyY", 89),
-    PlayPause(" ", "Space", 32),
-    Autoplay("k", "KeyK", 75),
-    ClearScreen("j", "KeyJ", 74)
+// 按键映射
+enum class Shortcut(val keyCode: Int) {
+    Previous(KeyEvent.KEYCODE_W), // 上一个视频
+    Next(KeyEvent.KEYCODE_S), // 下一个视频
+    LeftAction(KeyEvent.KEYCODE_R), // 左侧操作
+    RightAction(KeyEvent.KEYCODE_Z), // 右侧操作
+    OkAction(KeyEvent.KEYCODE_X), // 确认操作
+    PlayerFullscreen(KeyEvent.KEYCODE_H), // 播放器全屏
+    WebImmersive(KeyEvent.KEYCODE_Y), // 网页沉浸全屏
+    PlayPause(KeyEvent.KEYCODE_SPACE), // 播放/暂停
+    Autoplay(KeyEvent.KEYCODE_K), // 自动连播
+    ClearScreen(KeyEvent.KEYCODE_J) // 清屏
 }
 
 class ShortcutDispatcher(private val webView: WebView) {
     fun dispatch(shortcut: Shortcut) {
-        val key = JsEscaper.singleQuoted(shortcut.key)
-        val code = JsEscaper.singleQuoted(shortcut.code)
-        val js =
-            "(function(){" +
-                "var kc=${shortcut.legacyKeyCode};" +
-                "function patch(ev){" +
-                "try{" +
-                "Object.defineProperty(ev,'keyCode',{get:function(){return kc;}});" +
-                "Object.defineProperty(ev,'which',{get:function(){return kc;}});" +
-                "}catch(e){}" +
-                "}" +
-                "var o={key:'$key',code:'$code',bubbles:true,cancelable:true,composed:true};" +
-                "function blip(t){" +
-                "var ts=[document,window];" +
-                "if(document.body)ts.push(document.body);" +
-                "if(document.documentElement)ts.push(document.documentElement);" +
-                "var ae=document.activeElement;if(ae)ts.push(ae);" +
-                "for(var i=0;i<ts.length;i++){" +
-                "try{" +
-                "if(!ts[i]||!ts[i].dispatchEvent)continue;" +
-                "var ev=new KeyboardEvent(t,o);patch(ev);" +
-                "ts[i].dispatchEvent(ev);" +
-                "}catch(e){}" +
-                "}" +
-                "}" +
-                "blip('keydown');blip('keyup');" +
-                "})();"
-        webView.evaluateJavascript(js, null)
+        val downTime = SystemClock.uptimeMillis()
+        webView.dispatchKeyEvent(KeyEvent(downTime, downTime, KeyEvent.ACTION_DOWN, shortcut.keyCode, 0))
+        webView.dispatchKeyEvent(KeyEvent(downTime, downTime, KeyEvent.ACTION_UP, shortcut.keyCode, 0))
     }
 }
